9468e2de8cb2ddb5e039dcfa9c9e48350ba1f7a3
Daniel Bell <shinyhappydan@gmail.com>
2014-02-15 01:01:08 +0000

    Add helper methods for getting settings

--- build.sbt
+++ build.sbt
@@ -4,8 +4,8 @@ name := "zip"
 
 organization := "at.danisgre"
 
-version := "0.0.1"
+version := "0.0.3"
 
-publishMavenStyle := false
+publishMavenStyle := true
 
 publishTo := Some("Internal Springer maven repo" at "http://repo.tools.springer-sbm.com:8081/nexus/content/repositories/releases/")
--- src/main/scala/at/danisgre/sbt/zip/Zip.scala
+++ src/main/scala/at/danisgre/sbt/zip/Zip.scala
@@ -11,24 +11,33 @@ object Zip extends Plugin {
     dependentProjectsSettings ++
     buildDependenciesSettings ++
     ((artifactPath in zip)    :=  asZip((artifactPath in packageBin).value)) ++
-    (zip                      <<= newZipTask) ++
-    (newZipExtraFiles         :=  Seq.empty)
+    (zip                      <<= zipTask) ++
+    (zipExtraFile             :=  Seq.empty)
 
-  val zip                     = TaskKey[File]("zip", "New zip task")
-  val newZipExtraFiles        = SettingKey[Seq[(File, String)]]("newZipExtraFiles")
+  val zip                     = TaskKey[File]("zip", "Produces a zip with all dependencies of a configuration")
+  val zipExtraFile            = SettingKey[Seq[(File, String)]]("zipExtraFiles", "Extra files (and their paths) which should be added to the zip")
 
-  private def asZip(jarFile: File): File = {
-    file(jarFile.getAbsolutePath.replace(".jar", ".zip"))
+  def zipTaskForConfig(config: Configuration) = {
+    inConfig(config)(zipSettings)
   }
 
-  private def newZipTask = {
-    (allTheJars, (artifactPath in zip), newZipExtraFiles) map {
+  def zipTaskForConfigWithExtraFiles(config: Configuration, files: Seq[(File, String)]) = {
+    inConfig(config)(zipSettings) ++
+    ((zipExtraFile in config) := files)
+  }
+
+  private def zipTask = {
+    (allTheJars, artifactPath in zip, zipExtraFile) map {
       (packages, outputZip, extraFiles) => {
         val libs = packages.toSeq.get pair flatRebase("lib")
         IO.zip(libs ++ extraFiles, outputZip)
         outputZip
       }
     }
+  }
+
+  private def asZip(jarFile: File): File = {
+    file(jarFile.getAbsolutePath.replace(".jar", ".zip"))
   }
 }
 
